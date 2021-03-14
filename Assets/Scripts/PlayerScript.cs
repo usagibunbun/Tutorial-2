bae92fed85720cd01d22e5adffb26715ded37e98
usagibunbun <77363054+usagibunbun@users.noreply.github.com>
2021-03-14 14:43:33 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,14 +9,23 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rd2d;
     public float speed;
     public Text score;
+    public Text win;
+    public Text lives;
+    public Text lose;
+    private int livesValue = 3;
     private int scoreValue = 0;
-
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
     
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Coins: " + scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
+        win.text = "";
+        lose.text = "";
     }
 
     // Update is called once per frame
@@ -36,10 +45,43 @@ public class PlayerScript : MonoBehaviour
        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Coins: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
-        }
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector3 (36.0f, 1);
+                livesValue = 3;
+                lives.text = "Lives: " + livesValue.ToString();
 
+
+            }
+            if (scoreValue == 8)
+            {
+                win.text = "You Win! Game by Shayna Chevallier :}";
+                {
+                    musicSource.clip = musicClipOne;
+                    musicSource.Stop();
+
+                    musicSource.clip = musicClipTwo;
+                    musicSource.Play();
+                    musicSource.loop = false;
+                }
+            }
+            
+            
+        }
+        if (collision.collider.tag == "Enemy")
+            {
+                livesValue -= 1;
+                lives.text = "Lives: " + livesValue.ToString();
+                Destroy(collision.collider.gameObject);
+        
+            }
+            if (livesValue == 0)
+            {
+                lose.text = "You Lose! Game by Shayna Chevallier :{";
+                Destroy(gameObject);
+            }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
